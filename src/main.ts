58c89bb3effcf4cb1b21e007f4cc0b390c652c9c
ras0q <ras0q@users.noreply.github.com>
2024-05-26 07:21:34 +0900

    :sparkles: add settings

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 import { MarkdownView, Plugin } from "obsidian";
+import {
+	DEFAULT_SETTINGS,
+	SettingsTab,
+	type GraphBannerSettings,
+} from "./settings";
 
 export default class GraphBannerPlugin extends Plugin {
+	settings: GraphBannerSettings;
+
 	async onload() {
+		await this.loadSettings();
+		this.addSettingTab(new SettingsTab(this.app, this));
+
 		this.registerEvent(
 			this.app.workspace.on("file-open", async (file) => {
 				if (!file) return;
@@ -32,13 +42,26 @@ export default class GraphBannerPlugin extends Plugin {
 
 				const noteHeader = fileView.containerEl.querySelector(".inline-title");
 				const graphNode =
-					localGraphView.containerEl.querySelector(".view-content");
+					localGraphView.containerEl.querySelector<HTMLElement>(
+						".view-content",
+					);
 				if (
 					!noteHeader?.parentElement ||
 					!noteHeader?.nextSibling ||
 					!graphNode
 				) {
 					return;
+				}
+
+				graphNode.style.width = "auto";
+				graphNode.style.height = `${this.settings.bannerHeight}px`;
+				graphNode.style.marginBottom = "1rem";
+				if (!this.settings.displayGraphSettings) {
+					const graphControls =
+						graphNode.querySelector<HTMLElement>(".graph-controls");
+					if (graphControls) {
+						graphControls.style.display = "none";
+					}
 				}
 
 				noteHeader.parentElement.insertBefore(
@@ -69,5 +92,13 @@ export default class GraphBannerPlugin extends Plugin {
 				}
 			}),
 		);
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign(DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
 	}
 }
