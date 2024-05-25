faba2d0105121426c9a4ae3b8e1bd64143322414
ras0q <ras0q@users.noreply.github.com>
2024-05-26 05:41:19 +0900

    :recycle: prepare for the first release

--- scripts/version-bump.mjs
+++ scripts/version-bump.mjs
@@ -1,14 +1,21 @@
 import { readFileSync, writeFileSync } from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const targetVersion = process.env.npm_package_version;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // read minAppVersion from manifest.json and bump version to target version
-const manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
+const manifest = JSON.parse(
+	readFileSync(path.resolve(__dirname, "../manifest.json"), "utf8"),
+);
 const { minAppVersion } = manifest;
 manifest.version = targetVersion;
 writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
 
 // update versions.json with target version and minAppVersion from manifest.json
-const versions = JSON.parse(readFileSync("versions.json", "utf8"));
+const versions = JSON.parse(
+	readFileSync(path.resolve(__dirname, "../versions.json"), "utf8"),
+);
 versions[targetVersion] = minAppVersion;
 writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
