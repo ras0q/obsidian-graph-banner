f5bd8f11745c42826b4fd9a454aa0d7cea635b7f
ras0q <ras0q@users.noreply.github.com>
2024-05-26 15:00:29 +0900

    :boom: use style-settings plugin to manage css variables

--- src/main.ts
+++ src/main.ts
@@ -12,13 +12,24 @@ export default class GraphBannerPlugin extends Plugin {
 		await this.loadSettings();
 		this.addSettingTab(new SettingsTab(this.app, this));
 
+		// apply style settings
+		// https://github.com/mgmeyers/obsidian-style-settings?tab=readme-ov-file#plugin-support
+		this.app.workspace.trigger("parse-style-settings");
+
 		this.registerEvent(
 			this.app.workspace.on("file-open", async (file) => {
 				if (!file) {
 					throw new Error("Failed to get file");
 				}
 
-				const fileView = this.app.workspace.getActiveViewOfType(MarkdownView);
+				let fileView: MarkdownView | null = null;
+				for (let i = 0; i < 10; i++) {
+					fileView = this.app.workspace.getActiveViewOfType(MarkdownView);
+					if (fileView) {
+						break;
+					}
+					await new Promise((resolve) => setTimeout(resolve, 500));
+				}
 				if (!fileView || fileView.file !== file) {
 					throw new Error("Failed to get file view");
 				}
@@ -58,16 +69,12 @@ export default class GraphBannerPlugin extends Plugin {
 					throw new Error("Failed to get graph node");
 				}
 
-				graphNode.style.width = "auto";
-				graphNode.style.height = `${this.settings.bannerHeight}px`;
-				graphNode.style.marginBottom = "1rem";
-				if (!this.settings.displayGraphSettings) {
-					const graphControls = graphNode
-						.getElementsByClassName("graph-controls")
-						.item(0);
-					if (graphControls && graphControls instanceof HTMLElement) {
-						graphControls.style.display = "none";
-					}
+				graphNode.addClass("graph-banner-content");
+				const graphControls = graphNode
+					.getElementsByClassName("graph-controls")
+					.item(0);
+				if (graphControls && graphControls instanceof HTMLElement) {
+					graphControls.toggleClass("is-close", true);
 				}
 
 				noteHeader.parentElement.insertBefore(
